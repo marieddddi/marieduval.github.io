5f20361b34f3ab6b29a08a87dbccec9b11d674a6
marieddddi <124163576+marieddddi@users.noreply.github.com>
2023-03-21 11:16:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var refPop=null;
+
+// Initialise les variables globales
+function init()
+{
+    console.log("Fonction init");
+	refPop = document.getElementById("lettredemotiv"); 
+}
+
+//Zoom sur la photo
+function zoomIn (element) 
+{
+    element.style.transform = "scale(1.1)";
+}
+ 
+//Dezoom de la photo
+function zoomOut(element) 
+{
+    element.style.transform = "scale(1)";
+}
+
+//changement du nom du boutton et affichage de la lettre de motivation selon la situation
+function toggle_popup(button)
+{			
+	//refPop.style.display = "block"; //affiche ou cache le popup
+	if(refPop.style.display=="none")
+	{
+		button.value="cacher la lettre de motivation";
+		refPop.style.display="block";
+	}
+	else 
+	{
+		button.value="afficher la lettre de motivation";
+		refPop.style.display="none";
+	}
+}
+
